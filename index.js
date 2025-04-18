cfaa620c541dac48d3acba495b5b98ba8fbb14c2
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2025-04-18 13:58:12 +0600

    add file

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res)=>{
+    res.end('<h1 style="color:red;">Hello sumon</h1>');
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
