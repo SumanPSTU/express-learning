1c72c2112f3a41632d1483a60f3f1f0397041979
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2025-04-19 20:29:23 +0600

    update

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = 3000;
 app.get('/',(req,res)=>{
     res.end('<h1 style="color:red;">Hello sumon</h1>');
 });
+app.post('/',(req,res)=>{
+    res.end("hello");
+})
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
